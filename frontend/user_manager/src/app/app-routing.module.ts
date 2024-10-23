7d53d550013accee855425f4705941b02925be9b
Delca <nikoladelic9915@gmail.com>
2024-10-23 08:56:17 +0200

    Account component has been made

--- frontend/user_manager/src/app/app-routing.module.ts
+++ frontend/user_manager/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { Register2Component } from './register2/register2.component';
+import { AccountsComponent } from './accounts/accounts.component';
 
 const routes: Routes = [
   { path:"", redirectTo:"/home", pathMatch:"full"},
   { path:"register", component:RegisterComponent},
-  { path:"register2", component:Register2Component}
+  { path:"register2", component:Register2Component},
+  { path:"accounts", component:AccountsComponent}
 ];
 
 @NgModule({
--- frontend/user_manager/src/app/app.module.ts
+++ frontend/user_manager/src/app/app.module.ts
@@ -16,12 +16,14 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule} from "@angular/material/input";
 import { MatSelectModule} from "@angular/material/select";
 import { MatButtonModule} from "@angular/material/button";
+import { AccountsComponent } from './accounts/accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    Register2Component
+    Register2Component,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
