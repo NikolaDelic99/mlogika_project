6bfd135d16bbed0da83344a3234079cf54be48cf
Delca <nikoladelic9915@gmail.com>
2025-01-23 10:22:40 +0100

    SecurityConfig.java minor changes

--- backend/mlogika_spring/src/main/java/com/example/demo/SecurityConfig.java
+++ backend/mlogika_spring/src/main/java/com/example/demo/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
+
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -49,9 +52,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); 
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
-        configuration.setAllowedHeaders(List.of("*")); 
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); 
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
+        configuration.setAllowedHeaders(Arrays.asList("*")); 
         configuration.setAllowCredentials(true); 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
