4050e94d29f195d3539f640b9dfd8d098fc2e2f4
Delca <nikoladelic9915@gmail.com>
2025-02-08 10:45:34 +0100

    Deploy in trying...

--- backend/mlogika_spring/src/test/java/com/example/mlogika_spring/MlogikaSpringApplicationTests.java
+++ backend/mlogika_spring/src/test/java/com/example/mlogika_spring/MlogikaSpringApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.mlogika_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.demo.MlogikaSpringApplication;
+
+@SpringBootTest(classes = MlogikaSpringApplication.class)
 class MlogikaSpringApplicationTests {
 
 	@Test
